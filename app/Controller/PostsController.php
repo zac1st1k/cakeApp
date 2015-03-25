5fed01f994f9935f67c5f9d9cb6987e172c8f45d
zac1st1k <zacfirst@gmail.com>
2015-03-25 23:53:20 +1100

    + Post MVC

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,7 @@
+class PostsController extends AppController {
+    public $helpers = array('Html', 'Form');
+
+    public function index() {
+        $this->set('posts', $this->Post->find('all'));
+    }
+}
