625ec2510fb4b9efb5c36ab8f68aeddd8b550a10
zac1st1k <zacfirst@gmail.com>
2015-03-26 20:39:20 +1100

    + Login index

--- app/Model/User.php
+++ app/Model/User.php
@@ -26,5 +26,14 @@ class User extends AppModel {
         )
     );
 
-    
+    public function beforeSave($options = array()) {
+    if (isset($this->data[$this->alias]['password'])) {
+        $passwordHasher = new BlowfishPasswordHasher();
+        $this->data[$this->alias]['password'] = $passwordHasher->hash(
+            $this->data[$this->alias]['password']
+        );
+    }
+    return true;
+}
+
 }
